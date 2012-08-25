5661b0dd79ef84a874507b43bac99efb352b706a
megha55 <megha55@hotmail.com>
2012-08-25 16:29:44 +0530

    I/P file details

---
+++ files.h
@@ -0,0 +1,17 @@
+// Include header.h file
+#include "header.h"
+
+// Input file variables
+String room_file = "Rooms.out";
+String rollno_file = "RollNo.out";
+
+// Defining array size
+const int max_size = 200;
+const int min_size = 50;
+
+// fstream variables
+ifstream infile;    // For reading contents of file
+ofstream outfile;   // For writing contents into file
+
+// for looping variables
+int i, j, k;
