49a9c982044143f59c74aa543e94aeb5b1c5a6ae
megha55 <megha55@hotmail.com>
2012-09-08 06:26:57 +0530

    Main file

---
+++ SeatPlan-Main.cpp
@@ -0,0 +1,15 @@
+// Include SeatPlan.cpp for member fun. definitions
+#include "SeatPlan.cpp"
+
+// Main Function
+
+int main()
+{
+    // Creating objects of SeatPlan class
+    SeatPlan seatplan;
+    seatplan.get_details();
+    seatplan.show_details();
+    seatplan.valid();
+    
+    return 0;
+}
